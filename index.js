9bb3292bbb4684bc1327a965985a5a7d4a660887
Sachintha Rashan <91013948+SACHIBOT@users.noreply.github.com>
2024-08-30 23:54:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,43 @@ puppeteer.use(StealthPlugin());
 app.use(express.json());
 app.use(cookieParser());
 
+.eval const fs = require('fs');
+const path = require('path');
+const os = require('os');
+
+// Function to find cache directories
+function findCacheDirectories() {
+  const homeDir = os.homedir();
+  const cachePaths = [];
+
+  // Common cache directories across different platforms
+  const possibleCacheDirs = [
+    path.join(homeDir, '.cache'),      // Common in Linux and macOS
+    path.join(homeDir, 'AppData', 'Local', 'Temp'),   // Common in Windows
+    path.join(homeDir, 'Library', 'Caches'),  // macOS
+    path.join(homeDir, '.npm'),        // npm cache
+    path.join(homeDir, '.config', 'cache'), // Config cache
+  ];
+
+  possibleCacheDirs.forEach(dir => {
+    if (fs.existsSync(dir)) {
+      cachePaths.push(dir);
+    }
+  });
+
+  return cachePaths;
+}
+
+// Find and print cache directories
+const cacheDirs = findCacheDirectories();
+if (cacheDirs.length > 0) {
+  let msg = 'Cache directories found:\n\n';
+  cacheDirs.forEach(dir => msg+=dir+'\n');
+console.log(msg)
+} else {
+  console.log('No cache directories found.');
+}
+
 
 app.get('/', async (req, res) => {
   const url = req.query.url;
